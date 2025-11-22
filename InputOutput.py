1eefb26120be3aad46c0f95496f0ebdca9594abf
Anubhav Singh <165538800+anuvsingh@users.noreply.github.com>
2025-11-22 18:07:37 +0530

    Add files via upload

---
+++ InputOutput.py
@@ -0,0 +1,21 @@
+# OUTPUT
+print('Hello World');
+
+# Programmer I/O (Input/Output)
+n1 = 125;
+n2 = 600;
+print(n1 + n2)
+
+# User I/O
+num1 = (float)(input("Enter First Number: "))   # Type Casting - To change a data type to another different data type
+num2 = (float)(input("Enter Second Number: "))
+print(num1 + num2)
+
+# String
+first_name = input("Enter your first name: ")
+second_name = input("Enter your second name: ")
+print(first_name, second_name)
+print(first_name + second_name)
+
+
+
