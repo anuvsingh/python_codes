08cf84854f1142817c30b8912003134d45c2aa55
Anubhav Singh <165538800+anuvsingh@users.noreply.github.com>
2025-11-23 19:43:20 +0530

    Add files via upload

---
+++ 01_Basics/Basics.py
@@ -0,0 +1,44 @@
+# String Literals
+
+name1 = 'IronMan'
+
+name2 = "Captain's America"
+
+# Triple Quotes - Multiple line strings support
+name3 = """Hulk
+Smasher
+Black Widow
+Hawkaye
+Batman"""
+
+print(name1)
+print(name2)
+print(name3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01_Basics/dataType.py
@@ -0,0 +1,18 @@
+# Data Type
+name = "Hulk"
+num = 220
+dec = 22.15
+superhero = ["Superman", "Batman", "Hulk", "Goku"]
+menu = {"Dosa": 150, "Chicken Handi": 700, "Brownie": 100}
+
+print(type(name))
+print(type(num))
+print(type(dec))
+
+print(superhero)
+print(type(superhero))
+
+print(menu)
+print(type(menu))
+
+
