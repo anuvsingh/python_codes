4b344e0367b32c4e1fc4313b5a0000d1200ee73a
Anubhav Singh <165538800+anuvsingh@users.noreply.github.com>
2025-11-29 19:32:08 +0530

    Add files via upload

---
+++ 02_Operators/04_bitwise.py
@@ -0,0 +1,36 @@
+# AND (&) - Both switches should be ON. Dono he 1 hona chahiyea tbhi answer 1 hoga
+a = 15 # 01111
+b = 24 # 11000
+print (a & b)   # 01000 --> 8
+
+# OR (|) - ON, if any switch is ON. Ek bhi agr 1 hua toh final answer 1 hoga.
+n1 = 5 # 0101
+n2 = 3 # 0011
+print(n1 | n2)    # 0111 --> 7
+
+# XOR (^) - Dono he bits different hone chahiyea tbhi final value 1 hogi.
+num1 = 5 # 0101
+num2 = 3 # 0011
+print(num1 | num2)  # 0110 --> 6
+
+# NOT (~) - Flips the bit.
+c = 10 # 1010 --> ~c = -(c + 1)
+print(~c) # -6 (0110)
+
+# Left Shift(<<) - 1 bit left - multiplies by 2
+print(24<<1) # 110000 --> 48
+print(24<<2) # 1100000 --> 96
+print(-15<<1) # 
+
+# Right Shift(>>) - 1 bit right - divides by 2
+print(19>>1) # 01001
+print(-15>>1) # 1111 --> 16 ke niche wala 1 ka matlab negative sign hai --> -7
+
+
+
+
+
+
+
+
+
---
+++ 02_Operators/05_assignment.py
@@ -0,0 +1,18 @@
+n1 = 10
+n2 = 2
+
+n1+=n2
+print(n1) # 12
+
+n1-=n2 # n1 = n1 - n2 --> n1 = 12 - 2 = 10
+print(n1) # 10
+
+n1/=n2 # n1 = 5
+print(n1) # 5
+
+n1*=n2 # n1 = n1 * n2 --> 5 * 2 = 20
+print(n1) # 20
+
+n1%=n2 # modulus remainder nikal ke deta hai
+print(n1) # 20 % 2 = 0 remainder
+
